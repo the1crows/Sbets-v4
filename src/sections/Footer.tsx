77e1d49896a3bc62d7d8f842b40d9c2c75ace671
crow <113790120+the1crows@users.noreply.github.com>
2025-01-20 23:32:31 +0100

    Update Footer.tsx

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,41 +1,20 @@
-
-
-const LINKS = [
-  {
-    href: "https://t.me/SolBetsCasino",
-    title: "Telegram",
-  },
-  {
-    href: "https://twitter.com/SolBetsCasino",
-    title: "Twitter",
-  },
-  {
-    href: "https://www.solbets.app/assets/Whitepaper-97290c86.pdf",
-    title: "Whitepaper",
-  },
-];
-
-// Twitter link
-const TWITTER_LINK = {
-  href: "https://twitter.com/the1croweth",
-  title: "© 2024 SolBets Casino",
-};
-
+import { FOOTER_LINKS, FOOTER_TWITTER_LINK } from "../../constants";
+https://github.com/the1crows/Sbets-v4/tree/main/src/sections
 export default function Footer() {
   return (
-    <footer className="bg-black rounded-t-2xl shadow dark:bg-gray-900">
+    <footer className="bg-black border-t-2 border-[#5ce1e6] rounded-t-2xl shadow-lg">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <a
             target="_blank"
             rel="noopener noreferrer"
-            href="https://gamba.so/"
+            href="https://s-bets.fun"
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
-            <img src="/logo.png" className="h-10" alt="SolBets Logo" />
+            <img src="/logos.png" className="h-10" alt="Sbets Logo" />
           </a>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-            {LINKS.map((link) => (
+            {FOOTER_LINKS.map((link) => (
               <li key={link.href}>
                 <a
                   target="_blank"
@@ -51,8 +30,8 @@ export default function Footer() {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          <a href={TWITTER_LINK.href} className="hover:underline">
-            {TWITTER_LINK.title}
+          <a href={FOOTER_TWITTER_LINK.href} className="hover:underline">
+            {FOOTER_TWITTER_LINK.title}
           </a>
         </span>
       </div>
